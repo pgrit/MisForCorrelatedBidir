cb811a98059d0881f489fac0819d3157de7eba6e
Pascal Grittmann <s9pagrit@stud.uni-saarland.de>
2021-01-05 21:28:45 +0100

    better radius comparison figure

--- VcmExperiment/radiusfigure.py
+++ VcmExperiment/radiusfigure.py
@@ -3,28 +3,31 @@ import figuregen
 from figuregen import util
 import os
 
-crop_width = 90
-crop_height = 80
-
 scene_configs = [
-    # {
-    #     "scene_folder": "Results/ModernHall",
-    #     "scene_name": "ModernHall",
-    #     "crop": util.image.Cropbox(60, 400, crop_height, crop_width, 10),
-    #     "exposure": 1,
-    # },
+    {
+        "scene_folder": "Results/ModernHall",
+        "scene_name": "\\textsc{Modern Hall}",
+        "crop": util.image.Cropbox(60, 400, crop_height, crop_width, 10),
+        "exposure": 1,
+    },
     {
         "scene_folder": "Results/TargetPractice",
         "scene_name": "\\textsc{Target Practice}",
         "crop": util.image.Cropbox(40, 1000, crop_height, crop_width, 10),
         "exposure": 1.5,
     },
-    # {
-    #     "scene_folder": "Results/HomeOffice",
-    #     "scene_name": "HomeOffice",
-    #     "crop": util.image.Cropbox(160, 1140, crop_height, crop_width, 10),
-    #     "exposure": 1,
-    # },
+    {
+        "scene_folder": "Results/HomeOffice",
+        "scene_name": "\\textsc{Home Office}",
+        "crop": util.image.Cropbox(160, 1140, crop_height, crop_width, 10),
+        "exposure": 1,
+    },
+    {
+        "scene_folder": "Results/MinimalistWhiteRoom",
+        "scene_name": "\\textsc{Minimalist Room}",
+        "crop": util.image.Cropbox(30, 30, crop_height, crop_width, 10),
+        "exposure": 2,
+    },
     {
         "scene_folder": "Results/IndirectRoom",
         "scene_name": "\\textsc{Indirect Room}",
@@ -50,29 +53,23 @@ scene_configs = [
     #     "exposure": 0,
     # },
     # {
-    #     "scene_folder": "Results/MinimalistWhiteRoom",
-    #     "scene_name": "MinimalistWhiteRoom",
-    #     "crop": util.image.Cropbox(30, 30, crop_height, crop_width, 10),
-    #     "exposure": 0,
-    # },
-    # {
     #     "scene_folder": "Results/RoughGlasses",
     #     "scene_name": "RoughGlasses",
     #     "crop": util.image.Cropbox(30, 30, crop_height, crop_width, 10),
     #     "exposure": 0,
     # },
-    # {
-    #     "scene_folder": "Results/RoughGlassesIndirect",
-    #     "scene_name": "\\textsc{Rough Glasses}",
-    #     "crop": util.image.Cropbox(460, 320, crop_height, crop_width, 10),
-    #     "exposure": 2,
-    # }
+    {
+        "scene_folder": "Results/RoughGlassesIndirect",
+        "scene_name": "\\textsc{Rough Glasses}",
+        "crop": util.image.Cropbox(460, 320, crop_height, crop_width, 10),
+        "exposure": 2,
+    }
 ]
 
 methods = [
     ("$0.01r$", "Radius-x001"),
     ("$0.1r$", "Radius-x01"),
-    # ("$0.5r$", "Radius-x05"),
+    ("$0.5r$", "Radius-x05"),
     ("$r$", "PdfRatioFov"),
     ("$2r$", "Radius-x2"),
     ("$10r$", "Radius-x10"),
@@ -80,57 +77,65 @@ methods = [
     ("Balance", "Vcm"),
 ]
 
-def make(config, show_title):
-    method_images = [
-        pyexr.read(os.path.join(config["scene_folder"], folder, "render.exr"))
-        for _, folder in methods
+def make_plot():
+    rs = [1.0/100.0, 1.0/10.0, 0.5, 1, 2, 10, 100]
+    data = []
+    names = []
+
+    for config in scene_configs:
+        method_images = [
+            pyexr.read(os.path.join(config["scene_folder"], folder, "render.exr"))
+            for _, folder in methods
+        ]
+        reference_image = pyexr.read(os.path.join(config["scene_folder"], "reference.exr"))[:,:,:3]
+        scene_err = [
+            util.image.relative_mse_outlier_rejection(m, reference_image, 0.01)
+            for m in method_images
+        ]
+        speedups = [ err / scene_err[-1] for err in scene_err ]
+        speedups.pop()
+
+        data.append([rs, speedups])
+        names.append(config["scene_name"])
+
+    plot = figuregen.MatplotLinePlot(aspect_ratio=0.6, data=data)
+
+    plot.set_axis_label('x', "Radius")
+    plot.set_axis_label('y', "Error")
+
+    colors = [
+        [232, 181, 88],
+        [5, 142, 78],
+        [94, 163, 188],
+        [181, 63, 106],
+        [126, 83, 172],
+        [37, 85, 166],
     ]
-    reference_image = pyexr.read(os.path.join(config["scene_folder"], "reference.exr"))[:,:,:3]
+    plot.set_colors(colors)
+    plot.set_font(8, tex_package="{dfadobe}")
+    plot.set_linewidth(1.5)
 
-    # Compute error values
-    errors = [
-        # util.image.relative_mse(m, reference_image, 0.01)
-        util.image.relative_mse_outlier_rejection(m, reference_image, 0.01)
-        # util.image.smape(m, reference_image)
-        for m in method_images
-    ]
-    error_metric_name = "relMSE"
+    plot.set_axis_properties('x', ticks=[1.0/100.0, 1.0/10.0, 0.5, 1, 2, 10])
+    plot.set_axis_properties('y', ticks=[0.1, 0.5, 1.0], use_log_scale=False, range=[0.1, 1.2])
 
-    def error_string(index, array):
-        value = f"${array[index]:.2f}$ "
-        if index == len(methods)-1:
-            speedup = "($1.00\\times$)"
-        # elif index == 3:
-        #     speedup = "\\textbf{" + f"($\\mathbf{{ {array[index]/array[len(methods)-1]:.2f}\\times }}$)" + "}"
-        else:
-            speedup = f"(${array[index]/array[len(methods)-1]:.2f}\\times$)"
-        return "\\textsf{" + value + speedup + "}"
+    plot.set_v_line(pos=1, color=[0,0,0], linestyle=(-5,(4,6)), linewidth_pt=0.6)
 
-    def tonemap(img):
-        return figuregen.PNG(util.image.lin_to_srgb(util.image.exposure(img, config["exposure"])))
+    plot_module = figuregen.Grid(1,1)
+    plot_module.get_element(0,0).set_image(plot)
 
-    ref_grid = figuregen.Grid(1, 1)
-    ref_grid.get_element(0, 0).set_image(tonemap(reference_image))
-    ref_grid.get_element(0, 0).set_marker(config["crop"].get_marker_pos(), config["crop"].get_marker_size(), color=[255,255,255])
-    ref_grid.set_col_titles("bottom", [config["scene_name"]])
-    if show_title:
-        ref_grid.get_layout().set_col_titles("top", fontsize=8, field_size_mm=2.8, offset_mm=0.25)
-    ref_grid.get_layout().set_padding(right=1)
-    ref_grid.get_layout().set_col_titles("bottom", fontsize=8, field_size_mm=3, offset_mm=0.25)
+    reference_grid = figuregen.Grid(num_rows=2, num_cols=3)
+    for i in range(6):
+        e = reference_grid.get_element(int(i / 3), i % 3)
+        img = pyexr.read(os.path.join(scene_configs[i]["scene_folder"], "reference.exr"))[:,:,:3]
+        tm = figuregen.PNG(util.image.lin_to_srgb(util.image.exposure(img, scene_configs[i]["exposure"])))
+        e.set_image(tm)
+        e.set_caption(scene_configs[i]["scene_name"])
+        e.set_frame(3, colors[i])
+    l = reference_grid.get_layout()
+    l.set_caption(2.8, fontsize=7)
+    l.set_padding(left=1)
 
-    crop_grid = figuregen.Grid(num_cols=len(methods), num_rows=1)
-    for col in range(0, len(methods)):
-        crop_grid.get_element(0, col).set_image(tonemap(config["crop"].crop(method_images[col])))
-        crop_grid.get_element(0, col).set_caption(error_string(col, errors))
-    if show_title:
-        crop_grid.set_col_titles("top", [name for (name, _) in methods])
-        crop_grid.get_layout().set_col_titles("top", fontsize=8, field_size_mm=2.8, offset_mm=0.25)
-    crop_grid.get_layout().set_caption(height_mm=3, fontsize=8, offset_mm=0.25)
+    figuregen.figure([[plot_module, reference_grid]], 17.7, "Results/RadiusFigure.pdf",
+        tex_packages=["{dfadobe}"])
 
-    return [ref_grid, crop_grid]
-
-width_cm = 17.7
-rows = []
-for i in range(len(scene_configs)):
-    rows.append(make(scene_configs[i], i == 0))
-figuregen.figure(rows, width_cm, "Results/RadiusFigure.pdf", tex_packages=["{dfadobe}"])
+make_plot()
